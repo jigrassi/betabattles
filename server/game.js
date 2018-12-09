be260732c0550411cedc4bd3b573413e210a68f3
avenis <avenis@riotgames.com>
2018-12-08 19:28:23 -0800

    gluing client and server code together

--- server/game.js
+++ server/game.js
@@ -1,15 +1,41 @@
 module.exports = {
+
     p1: null,
     p2: null,
+    serverState: null,
+    playersById: new Map(),
 
-    init_players: function(p1, p2) {
+    init: function(p1, p2) {
         this.p1 = p1;
         this.p2 = p2;
+        this.playersById[p1.id] = p1;
+        this.playersById[p2.id] = p2;
+        ServerState = require('./ServerState.js');
+        this.serverState = new ServerState();
     },
 
     fuck: function(x) {
         this.p1.emit('fuck', x - 1);
         this.p2.emit('fuck', x);
+    },
+
+    setArmyStance(id, stance) {
+        player = this.playersById[id];
+        serverState.setPlayerArmyStance(id, stance);
+        p1.emit('update', serverState.createPlayerState());
+        p2.emit('update', serverState.createPlayerState());
+    },
+
+    increaseIncome(id) {
+        player = this.playersById[id];
+        serverState.increaseIncome(id);
+        player.emit('update', serverState.createPlayerState());
+    },
+
+    increaseArmy(id) {
+        player = this.playersById[id];
+        serverState.increaseArmy(id);
+        player.emit('update', serverState.createPlayerState());
     },
 
     disconnect_all: function() {
