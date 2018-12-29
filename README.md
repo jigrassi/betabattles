3ee03c8739c8eb15a42bc262daef4875ae4a2deb
rachelvenis <rachelvenis@gmail.com>
2018-12-29 18:51:02 -0500

    removed requireJS dependency

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ uses the following tech:
 	node for web server base
 	express for routing
 	socket.io for client/server communication
-	requireJS for dependency management
+	ES6 stuff (imports, classes)
+
+to run:
+	clone this repo, and yiboduan and jayme
+	in yiboduan root run `node app.js` except to run it on a port that isn't 80 you'll need to change the port variable in `app.js`
--- client/ClientState.js
+++ client/ClientState.js
@@ -1,62 +1,60 @@
-define(function() {
-
-    return class ClientState {
-        constructor(server) {
-            this.income = 5;
-            this.funds = 0;
-            this.myArmy = 0;
-            this.oppArmy = 'unknown';
-            this.myArmyStance = 'passive'; //'aggressive'
-            this.oppArmyStance = 'passive';
-            this.myBase = 100;
-            this.oppBase = 100;
-            this.readyState = false;
-            this.oppReadyState = false;
-            this.myUsername = '';
-            this.oppUsername = '';
-            this.oppPresent = false;
-            this.youWin = false;
-            this.gameEnd = false;
-            this.myWins = 0;
-            this.oppWins = 0;
-        }
-
-        update(playerState) {
-            this.income = playerState.income;
-            this.funds = playerState.funds;
-            this.myArmy = playerState.myArmy;
-            this.oppArmy = playerState.oppArmy;
-            this.myArmyStance = playerState.myArmyStance;
-            this.oppArmyStance = playerState.oppArmyStance;
-            this.myBase = playerState.myBase;
-            this.oppBase = playerState.oppBase;
-            this.youWin = playerState.youWin;
-            this.gameEnd = playerState.gameEnd;
-            this.myWins = playerState.myWins;
-            this.oppWins = playerState.oppWins;
-        }
-
-        setArmyStance(stance) {
-            myArmyStance = stance;
-            server.setPlayerStance(stance);
-        }
-
-        increaseIncome() {
-            server.increasePlayerFunds();
-        }
-
-        increaseArmy() {
-            server.increaseArmy();
-        }
-
-        updateReadyState(readyStateMap) {
-            this.readyState = readyStateMap.self;
-            this.oppReadyState = readyStateMap.opp;
-        }
-
-        updateUsernames(usernameMap) {
-            this.myUsername = usernameMap.self;
-            this.oppUsername = usernameMap.opp;
-        }
+class ClientState {
+    constructor(server) {
+        this.income = 5;
+        this.funds = 0;
+        this.myArmy = 0;
+        this.oppArmy = 'unknown';
+        this.myArmyStance = 'passive'; //'aggressive'
+        this.oppArmyStance = 'passive';
+        this.myBase = 100;
+        this.oppBase = 100;
+        this.readyState = false;
+        this.oppReadyState = false;
+        this.myUsername = '';
+        this.oppUsername = '';
+        this.oppPresent = false;
+        this.youWin = false;
+        this.gameEnd = false;
+        this.myWins = 0;
+        this.oppWins = 0;
     }
-});
+
+    update(playerState) {
+        this.income = playerState.income;
+        this.funds = playerState.funds;
+        this.myArmy = playerState.myArmy;
+        this.oppArmy = playerState.oppArmy;
+        this.myArmyStance = playerState.myArmyStance;
+        this.oppArmyStance = playerState.oppArmyStance;
+        this.myBase = playerState.myBase;
+        this.oppBase = playerState.oppBase;
+        this.youWin = playerState.youWin;
+        this.gameEnd = playerState.gameEnd;
+        this.myWins = playerState.myWins;
+        this.oppWins = playerState.oppWins;
+    }
+
+    setArmyStance(stance) {
+        myArmyStance = stance;
+        server.setPlayerStance(stance);
+    }
+
+    increaseIncome() {
+        server.increasePlayerFunds();
+    }
+
+    increaseArmy() {
+        server.increaseArmy();
+    }
+
+    updateReadyState(readyStateMap) {
+        this.readyState = readyStateMap.self;
+        this.oppReadyState = readyStateMap.opp;
+    }
+
+    updateUsernames(usernameMap) {
+        this.myUsername = usernameMap.self;
+        this.oppUsername = usernameMap.opp;
+    }
+}
+export default ClientState;
